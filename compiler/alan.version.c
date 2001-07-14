8f0e24beaaf8d55bf4e9c8d55b115130c7154917
Thomas Nilsson <>
2001-07-14 21:07:53 +0000

    Added version files to the repository

---
+++ compiler/alan.version.c
@@ -0,0 +1,19 @@
+/* alan.c - Created by venum 4.2 beta */
+
+#include "alan.version.h"
+
+Product alan = {
+  "Alan",
+  "Adventure Language System",
+  "Alan 2.8(6)",
+  "Alan 2.8(6) -- Adventure Language System (2001-07-14 07:47)",
+  "2001-07-14",
+  "07:47:54",
+  "Thomas Nilsson",
+  "",
+  "cygwin32",
+  {"2.8(6)", 2, 8, 6, 995096874, ""}
+};
+
+static char *alanId =
+  "@(#)RELEASE ";
