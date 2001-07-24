86bcc1de941ac169f34dff9152d0b07796bf952a
Thomas Nilsson <>
2001-07-24 21:27:11 +0000

    Reverted to Cygwin and discarded the readline experiments for Windows

---
+++ compiler/wrdTest.c
@@ -0,0 +1,20 @@
+/*======================================================================*\
+
+  wrdTest.c
+
+  Unit tests for WRD node in the Alan compiler
+
+\*======================================================================*/
+
+#include "wrd.c"
+
+void testInitWrd()
+{
+}
+
+
+void registerWrdUnitTests()
+{
+  registerUnitTest(testInitWrd);
+}
+
