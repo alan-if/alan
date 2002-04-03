0d2c747b177bfb09ebea97aaf05dc4bbfa8b623b
Thomas Nilsson <>
2002-04-03 19:27:24 +0000

    *** empty log message ***

---
+++ interpreter/exeTest.c
@@ -0,0 +1,45 @@
+/*======================================================================*\
+
+  exeTest.c
+
+  Unit tests for exe module in the Alan interpreter
+
+\*======================================================================*/
+
+#include "exe.c"
+
+static char testFileName[] = "getStringTestFile";
+static FILE *testFile;
+static void writeAndOpenGetStringTestFile(int fpos, char *testString)
+{
+  int i;
+
+  testFile = fopen(testFileName, "wb");
+  for (i = 0; i < fpos; i++) fputc(' ', testFile);
+  fprintf(testFile, testString);
+  fclose(testFile);
+  txtfil = fopen(testFileName, "rb");
+}
+
+void testGetString()
+{
+  int fpos = 55;
+  char testString[] = "hejhopp";
+
+  writeAndOpenGetStringTestFile(fpos, testString);
+  header->pack = FALSE;
+  header->stringOffset = 0;
+  getstr(fpos, strlen(testString));
+  unitAssert(strcmp((char *)pop(), testString)==0);
+  header->stringOffset = 1;
+  getstr(fpos, strlen(testString)-1);
+  unitAssert(strcmp((char *)pop(), &testString[1])==0);
+  fclose(txtfil);
+  unlink(testFileName);
+}
+
+
+void registerExeUnitTests()
+{
+  registerUnitTest(testGetString);
+}
