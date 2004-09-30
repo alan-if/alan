4d4732447a8b1e9450e7f25fa2be45df1ccd65bb
Thomas Nilsson <>
2004-09-30 06:17:01 +0000

    Fixed WinArun icon again, this time without multiple Font toolbars ;-)

--- interpreter/resources.h
+++ interpreter/resources.h
@@ -1,14 +1,12 @@
 #define IDC_STATIC (-1)
 
-#define IDR_ARUN                        30020
+#define IDR_ARUN                        25000
+#define IDD_ABOUT                       25008
 
-#define IDI_ARUN                        30020
-#define IDD_ABOUT                       30004
-
-#define ID_MENU_RESTART			31001
-#define ID_MENU_SAVE			31002
-#define ID_MENU_RECORD			31003
-#define ID_MENU_PLAYBACK		31004
-#define ID_MENU_TRANSCRIPT		31005
-#define ID_MENU_RESTORE			31006
-#define ID_MENU_ABOUT                   31007
+#define ID_MENU_RESTART			25001
+#define ID_MENU_SAVE			25002
+#define ID_MENU_RECORD			25003
+#define ID_MENU_PLAYBACK		25004
+#define ID_MENU_TRANSCRIPT		25005
+#define ID_MENU_RESTORE			25006
+#define ID_MENU_ABOUT                   25007
