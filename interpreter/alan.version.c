db79754848d88c76c77d24202b9b52db03aae93e
Thomas Nilsson <>
2005-07-08 23:38:28 +0000

    *** empty log message ***

---
+++ interpreter/alan.version.c
@@ -0,0 +1,19 @@
+/* alan.c - Created by venum 4.2 beta */
+
+#include "alan.version.h"
+
+Product alan = {
+  "Alan",
+  "Adventure Language System",
+  "Alan 3.0.dev35",
+  "Alan 3.0.dev35 -- Adventure Language System (2005-06-12 19:50)",
+  "2005-06-12",
+  "19:50:31",
+  "thoni",
+  "",
+  "cygwin32",
+  {"3.0.dev35", 3, 0, 35, 1118598631, "dev"}
+};
+
+static char *alanId =
+  "@(#)RELEASE ";
