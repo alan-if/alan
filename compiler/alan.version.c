042b14f5d468d94a421c93f24fdcea7106fe310b
Thomas Nilsson <>
2005-09-10 12:42:36 +0000

    *** empty log message ***

--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -5,14 +5,14 @@
 Product alan = {
   "Alan",
   "Adventure Language System",
-  "Alan 3.0.dev37",
-  "Alan 3.0.dev37 -- Adventure Language System (2005-08-27 01:49)",
-  "2005-08-27",
-  "01:49:05",
+  "Alan 3.0alpha1",
+  "Alan 3.0alpha1 -- Adventure Language System (2005-09-10 14:40)",
+  "2005-09-10",
+  "14:40:23",
   "thoni",
   "",
   "cygwin32",
-  {"3.0.dev37", 3, 0, 37, 1125100145, "dev"}
+  {"3.0alpha1", 3, 0, 1, 1126356023, "alpha"}
 };
 
 static char *alanId =
--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -5,14 +5,14 @@
 Product alan = {
   "Alan",
   "Adventure Language System",
-  "Alan 3.0.dev37",
-  "Alan 3.0.dev37 -- Adventure Language System (2005-09-10 09:26)",
+  "Alan 3.0alpha1",
+  "Alan 3.0alpha1 -- Adventure Language System (2005-09-10 14:40)",
   "2005-09-10",
-  "09:26:26",
+  "14:40:23",
   "thoni",
   "",
   "cygwin32",
-  {"3.0.dev37", 3, 0, 37, 1126337186, "dev"}
+  {"3.0alpha1", 3, 0, 1, 1126356023, "alpha"}
 };
 
 static char *alanId =
