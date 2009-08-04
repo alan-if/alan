1d30ab9eecc75bde0553ad2072e8e0b6d59352b0
Thomas Nilsson <>
2009-08-04 14:41:55 +0000

    *** empty log message ***

---
+++ alan.version.c
@@ -0,0 +1,19 @@
+/* alan.c - Created by venum 4.2 beta */
+
+#include "alan.version.h"
+
+Product alan = {
+  "Alan",
+  "Adventure Language System",
+  "Alan 3.0alpha8",
+  "Alan 3.0alpha8 -- Adventure Language System (2009-08-04 01:03)",
+  "2009-08-04",
+  "01:03:09",
+  "thoni",
+  "",
+  "cygwin32",
+  {"3.0alpha8", 3, 0, 8, 1249340589, "alpha"}
+};
+
+static char *alanId =
+  "@(#)RELEASE ";
