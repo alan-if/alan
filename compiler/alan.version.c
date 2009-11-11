30cd027eb38dd611d0d915646b0ff954dda09ec4
thoni\Thomas <>
2009-11-11 15:48:41 +0000

    Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0alpha8",
-  "Alan 3.0alpha8 -- Adventure Language System (2009-08-30 21:47)",
-  "2009-08-30",
-  "21:47:46",
-  "thoni",
+  "Alan 3.0alpha8 -- Adventure Language System (2009-11-07 17:36)",
+  "2009-11-07",
+  "17:36:43",
+  "Thomas",
   "",
   "cygwin32",
-  {"3.0alpha8", 3, 0, 8, 1251661666, "alpha"}
+  {"3.0alpha8", 3, 0, 8, 1257611803, "alpha"}
 };
 
 static char *alanId =
