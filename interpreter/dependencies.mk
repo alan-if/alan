1d3380ad478a91d4d902e60de5ddbd7c8ae6a8e7
thoni <>
2010-06-14 15:29:45 +0000

    Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- interpreter/dependencies.mk
+++ interpreter/dependencies.mk
@@ -1,131 +1,54 @@
-unittests.o: unittests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h
-AltInfoTests.o: AltInfoTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h AltInfo.c AltInfo.h types.h \
-  sysdep.h acode.h params.h ParameterPosition.h checkentry.h debug.h \
-  inter.h stack.h lists.h instance.h set.h options.h memory.h current.h \
-  class.h literal.h
-StateStackTests.o: StateStackTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h StateStack.c StateStack.h types.h \
+unittests.o: unittests.c
+AltInfoTests.o: AltInfoTests.c AltInfo.c AltInfo.h types.h sysdep.h \
+  acode.h params.h ParameterPosition.h checkentry.h debug.h inter.h \
+  stack.h lists.h instance.h set.h options.h memory.h current.h class.h \
+  literal.h
+StateStackTests.o: StateStackTests.c StateStack.c StateStack.h types.h \
   sysdep.h acode.h syserr.h memory.h instance.h set.h event.h
-argsTests.o: argsTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h args.c sysdep.h args.h types.h \
-  acode.h options.h memory.h utils.h winargs.c
-actTests.o: actTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h act.c act.h params.h types.h \
-  sysdep.h acode.h AltInfo.h ParameterPosition.h output.h msg.h lists.h
-exeTests.o: exeTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h exe.c exe.h sysdep.h acode.h \
-  types.h set.h lists.h state.h syserr.h term.h utils.h instance.h \
-  inter.h stack.h decode.h save.h memory.h output.h score.h event.h \
-  current.h word.h msg.h params.h actor.h readline.h class.h container.h
-interTests.o: interTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h inter.c inter.h types.h sysdep.h \
-  acode.h stack.h current.h exe.h set.h syserr.h debug.h options.h save.h \
+argsTests.o: argsTests.c args.c sysdep.h args.h types.h acode.h options.h \
+  memory.h utils.h unixargs.c
+actTests.o: actTests.c act.c act.h params.h types.h sysdep.h acode.h \
+  AltInfo.h ParameterPosition.h output.h msg.h lists.h
+exeTests.o: exeTests.c exe.c exe.h sysdep.h acode.h types.h set.h lists.h \
+  state.h syserr.h term.h utils.h instance.h inter.h stack.h decode.h \
+  save.h memory.h output.h score.h event.h current.h word.h msg.h \
+  params.h actor.h readline.h class.h container.h
+interTests.o: interTests.c inter.c inter.h types.h sysdep.h acode.h \
+  stack.h current.h exe.h set.h syserr.h debug.h options.h save.h \
   memory.h output.h score.h params.h instance.h Container.h
-mainTests.o: mainTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h main.c main.h types.h sysdep.h \
-  acode.h state.h lists.h syserr.h options.h utils.h args.h parse.h \
-  params.h scan.h inter.h stack.h rules.h reverse.h debug.h exe.h set.h \
-  term.h instance.h memory.h output.h Container.h dictionary.h class.h \
-  score.h decode.h msg.h event.h syntax.h current.h literal.h readline.h \
-  alan.version.h version.h
-parseTests.o: parseTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h parse.c parse.h types.h sysdep.h \
-  acode.h params.h AltInfo.h ParameterPosition.h inter.h stack.h \
-  current.h act.h term.h lists.h options.h syserr.h Location.h instance.h \
-  set.h memory.h output.h dictionary.h syntax.h word.h msg.h literal.h \
+mainTests.o: mainTests.c main.c main.h types.h sysdep.h acode.h state.h \
+  lists.h syserr.h options.h utils.h args.h parse.h params.h scan.h \
+  inter.h stack.h rules.h reverse.h debug.h exe.h set.h term.h instance.h \
+  memory.h output.h Container.h dictionary.h class.h score.h decode.h \
+  msg.h event.h syntax.h current.h literal.h readline.h alan.version.h \
+  version.h
+parseTests.o: parseTests.c parse.c parse.h types.h sysdep.h acode.h \
+  params.h AltInfo.h ParameterPosition.h inter.h stack.h current.h act.h \
+  term.h lists.h options.h syserr.h Location.h instance.h set.h memory.h \
+  output.h dictionary.h syntax.h word.h msg.h literal.h \
   parameterPosition.h scan.h
-paramsTests.o: paramsTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h params.c params.h types.h \
-  sysdep.h acode.h lists.h memory.h literal.h ParameterPosition.h
-parameterPositionTests.o: parameterPositionTests.c \
-  ../cgreen/include/cgreen/cgreen.h ../cgreen/include/cgreen/unit.h \
-  ../cgreen/include/cgreen/reporter.h ../cgreen/include/cgreen/mocks.h \
-  ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h parameterPosition.c \
+paramsTests.o: paramsTests.c params.c params.h types.h sysdep.h acode.h \
+  lists.h memory.h literal.h ParameterPosition.h
+parameterPositionTests.o: parameterPositionTests.c parameterPosition.c \
   parameterPosition.h acode.h types.h sysdep.h params.h memory.h lists.h
-reverseTests.o: reverseTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h reverse.c reverse.h types.h \
-  sysdep.h acode.h lists.h checkentry.h rules.h msg.h params.h
-saveTests.o: saveTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h save.c save.h current.h types.h \
-  sysdep.h acode.h lists.h readline.h options.h syserr.h instance.h set.h \
-  memory.h output.h args.h score.h event.h msg.h params.h
-setTests.o: setTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h set.c set.h acode.h types.h \
-  sysdep.h lists.h syserr.h memory.h instance.h
-stackTests.o: stackTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h stack.c stack.h acode.h types.h \
-  sysdep.h syserr.h memory.h
-stateTests.o: stateTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h state.c state.h types.h sysdep.h \
-  acode.h syserr.h current.h word.h StateStack.h instance.h set.h \
-  attribute.h memory.h score.h event.h lists.h
-sysdepTests.o: sysdepTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h sysdep.c sysdep.h syserr.h
-outputTests.o: outputTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h output.c output.h types.h \
-  sysdep.h acode.h options.h memory.h word.h lists.h term.h syserr.h \
-  dictionary.h current.h msg.h params.h readline.h instance.h set.h
-wordTests.o: wordTests.c ../cgreen/include/cgreen/cgreen.h \
-  ../cgreen/include/cgreen/unit.h ../cgreen/include/cgreen/reporter.h \
-  ../cgreen/include/cgreen/mocks.h ../cgreen/include/cgreen/constraint.h \
-  ../cgreen/include/cgreen/text_reporter.h \
-  ../cgreen/include/cgreen/assertions.h word.c word.h types.h sysdep.h \
-  acode.h memory.h syserr.h lists.h
+reverseTests.o: reverseTests.c reverse.c reverse.h types.h sysdep.h \
+  acode.h lists.h checkentry.h rules.h msg.h params.h
+saveTests.o: saveTests.c save.c save.h current.h types.h sysdep.h acode.h \
+  lists.h readline.h options.h syserr.h instance.h set.h memory.h \
+  output.h args.h score.h event.h msg.h params.h
+setTests.o: setTests.c set.c set.h acode.h types.h sysdep.h lists.h \
+  syserr.h memory.h instance.h
+stackTests.o: stackTests.c stack.c stack.h acode.h types.h sysdep.h \
+  syserr.h memory.h
+stateTests.o: stateTests.c state.c state.h types.h sysdep.h acode.h \
+  syserr.h current.h word.h StateStack.h instance.h set.h attribute.h \
+  memory.h score.h event.h lists.h
+sysdepTests.o: sysdepTests.c sysdep.c sysdep.h syserr.h
+outputTests.o: outputTests.c output.c output.h types.h sysdep.h acode.h \
+  options.h memory.h word.h lists.h term.h syserr.h dictionary.h \
+  current.h msg.h params.h readline.h instance.h set.h
+wordTests.o: wordTests.c word.c word.h types.h sysdep.h acode.h memory.h \
+  syserr.h lists.h
 Container.o: Container.c Container.h types.h sysdep.h acode.h instance.h \
   set.h syserr.h inter.h stack.h lists.h memory.h current.h msg.h \
   params.h output.h
@@ -180,7 +103,7 @@ AltInfo.o: AltInfo.c AltInfo.h types.h sysdep.h acode.h params.h \
 StateStack.o: StateStack.c StateStack.h types.h sysdep.h acode.h syserr.h \
   memory.h
 args.o: args.c sysdep.h args.h types.h acode.h options.h memory.h utils.h \
-  winargs.c
+  unixargs.c
 act.o: act.c act.h params.h types.h sysdep.h acode.h AltInfo.h \
   ParameterPosition.h output.h msg.h
 exe.o: exe.c exe.h sysdep.h acode.h types.h set.h lists.h state.h \
