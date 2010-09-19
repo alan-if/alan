9176ee9711a20bb771328350560a371b540d6a2e
thoni <>
2010-09-19 10:38:45 +0000

    Added a common Makefile for version and dependency generation rules

---
+++ common.mk
@@ -0,0 +1,30 @@
+###################################################################
+#
+# Version number file generation
+#
+alan.version.h : ../alan.version.h
+	cp ../alan.version.h .
+
+alan.version.c:  ../alan.version.c  $(VERSIONSRCS)
+	cp ../alan.version.c .
+
+version.h : ../version.h
+	cp ../version.h .
+
+../alan.version.c ../alan.version.h ../version.h:
+	cd ..; venum alan time
+
+../alan.version:
+	cd ..; cvs update alan.version
+
+
+###################################################################
+# Automatically generate dependency rules 
+%.d : %.c 
+	$(CC) $(CCFLAGS) -MF"$@" -MG -MM -MP -MT"$@" -MT"$(<:.c=.o)" "$<" 
+
+# -MF  write the generated dependency rule to a file 
+# -MG  assume missing headers will be generated and don't stop with an error 
+# -MM  generate dependency rule for prerequisite, skipping system headers 
+# -MP  add phony target for each header to prevent errors when header is missing 
+# -MT  add a target to the generated dependency 
