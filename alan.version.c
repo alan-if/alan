b1b30daaef874f9ac315344194c0401d9fa9e496
thoni <>
2010-10-03 23:50:40 +0000

    Finally going for beta 1

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta1",
-  "Alan 3.0beta1 -- Adventure Language System (2010-09-27 02:36)",
-  "2010-09-27",
-  "02:36:07",
+  "Alan 3.0beta1 -- Adventure Language System (2010-10-04 01:50)",
+  "2010-10-04",
+  "01:50:07",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta1", 3, 0, 1, 1285547767, "beta"}
+  {"3.0beta1", 3, 0, 1, 1286149807, "beta"}
 };
 
 static char *alanId =
