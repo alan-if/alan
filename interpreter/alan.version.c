519f774da7c18d8d633a5593a3b50c0994582cb4
thoni <>
2010-12-19 14:10:19 +0000

    Ensured that iso8859-1.alan is actually stored as that Added a localization chapter in the manual

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta1",
-  "Alan 3.0beta1 -- Adventure Language System (2010-12-14 18:42)",
+  "Alan 3.0beta1 -- Adventure Language System (2010-12-14 19:09)",
   "2010-12-14",
-  "18:42:59",
+  "19:09:12",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta1", 3, 0, 1, 1292348579, "beta"}
+  {"3.0beta1", 3, 0, 1, 1292350152, "beta"}
 };
 
 static char *alanId =
