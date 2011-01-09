aa3ad3a2e36d2bf1a84f89c8de516395a0966cd8
thoni <>
2011-01-09 15:39:53 +0000

    Added the tests for transcript to cygwintests

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta1",
-  "Alan 3.0beta1 -- Adventure Language System (2011-01-08 21:13)",
-  "2011-01-08",
-  "21:13:04",
+  "Alan 3.0beta1 -- Adventure Language System (2011-01-09 16:35)",
+  "2011-01-09",
+  "16:35:27",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta1", 3, 0, 1, 1294517584, "beta"}
+  {"3.0beta1", 3, 0, 1, 1294587327, "beta"}
 };
 
 static char *alanId =
