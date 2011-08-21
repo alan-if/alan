fe90f17ed1af72572861ccdb83193ad92f8f24de
thoni <>
2011-08-21 17:32:51 +0000

    Fixed a v2 source sample creeping in...

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta2",
-  "Alan 3.0beta2 -- Adventure Language System (2011-08-19 19:30)",
-  "2011-08-19",
-  "19:30:57",
+  "Alan 3.0beta2 -- Adventure Language System (2011-08-21 19:04)",
+  "2011-08-21",
+  "19:04:42",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta2", 3, 0, 2, 1313775057, "beta"}
+  {"3.0beta2", 3, 0, 2, 1313946282, "beta"}
 };
 
 static char *alanId =
