95867494605b5747ee2c2340eb313588108c7bf7
thoni <>
2011-12-17 12:08:31 +0000

    *** empty log message ***

---
+++ interpreter/instanceTests.c
@@ -0,0 +1,51 @@
+#include "cgreen/cgreen.h"
+#include "cgreen/mocks.h"
+
+#include "instance.c"
+
+static ACodeHeader localHeader;
+
+void setUp() {
+    header = &localHeader;
+    literals = NULL;
+}
+
+
+void tearDown() {
+    free(literals);
+}
+
+
+static void given_a_literal_table_of(int size) {
+    literals = allocate(sizeof(LiteralEntry)*(size+1));
+}
+
+static void given_that_literal_has_value(int literal_number, int value) {
+    literals[literal_number].value = value;
+}
+
+
+Ensure canAccessLiteralValue() {
+    int value = 45;
+    int literal_number = 1;
+    
+    int attribute_number_for_literal_value = 1;
+
+    given_a_literal_table_of(1);
+    given_that_literal_has_value(1, value);
+
+    assert_equal(value, literalAttribute(instanceFromLiteral(literal_number), attribute_number_for_literal_value));
+}
+
+TestSuite *instanceTests(void)
+{
+    TestSuite *suite = create_test_suite();
+
+    setup(suite, setUp);
+    
+    add_test(suite, canAccessLiteralValue);
+
+    teardown(suite, tearDown);
+
+    return suite;
+}
