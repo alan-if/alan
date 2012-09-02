1cea4b7150a4e6d129a5f6d99363c8bde714e56d
thoni <>
2012-09-02 20:17:34 +0000

    Removed the faked failed unittest

--- interpreter/actTests.c
+++ interpreter/actTests.c
@@ -5,12 +5,7 @@
 
 #include "lists.h"
 
-Ensure(fails) {
-    assert_true(false);
-}
-
 TestSuite *actTests() {
     TestSuite *suite = create_test_suite();
-    add_test(suite, fails);
     return suite;
 }
