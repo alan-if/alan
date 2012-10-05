503373804c99d509ca0b12f4f417f02f965a54d6
thoni <>
2012-10-05 15:42:16 +0000

    *** empty log message ***

--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta3",
-  "Alan 3.0beta3 -- Adventure Language System (2012-10-03 22:45)",
-  "2012-10-03",
-  "22:45:00",
+  "Alan 3.0beta3 -- Adventure Language System (2012-10-05 16:12)",
+  "2012-10-05",
+  "16:12:18",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta3", 3, 0, 3, 1349297100, "beta"}
+  {"3.0beta3", 3, 0, 3, 1349446338, "beta"}
 };
 
 static char *alanId =
--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta3",
-  "Alan 3.0beta3 -- Adventure Language System (2012-10-02 21:58)",
-  "2012-10-02",
-  "21:58:41",
+  "Alan 3.0beta3 -- Adventure Language System (2012-10-05 16:11)",
+  "2012-10-05",
+  "16:11:38",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta3", 3, 0, 3, 1349207921, "beta"}
+  {"3.0beta3", 3, 0, 3, 1349446298, "beta"}
 };
 
 static char *alanId =
