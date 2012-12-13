728a33ddc16501ddc3fcd2b3d79c62323d579646
thoni <>
2012-12-13 13:47:04 +0000

    *** empty log message ***

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta3",
-  "Alan 3.0beta3 -- Adventure Language System (2012-11-27 20:06)",
-  "2012-11-27",
-  "20:06:19",
+  "Alan 3.0beta3 -- Adventure Language System (2012-12-13 14:44)",
+  "2012-12-13",
+  "14:44:15",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta3", 3, 0, 3, 1354043179, "beta"}
+  {"3.0beta3", 3, 0, 3, 1355406255, "beta"}
 };
 
 static char *alanId =
