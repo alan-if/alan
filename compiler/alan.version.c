7cb5843be958cbf642d8701f78fe20e30104e391
Thomas Nilsson <thomas@junovagen.se>
2013-12-18 08:47:28 +0000

    Generated new TMK-files

--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta3",
-  "Alan 3.0beta3 -- Adventure Language System (2013-12-09 19:02)",
-  "2013-12-09",
-  "19:02:41",
+  "Alan 3.0beta3 -- Adventure Language System (2013-12-18 09:43)",
+  "2013-12-18",
+  "09:43:08",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta3", 3, 0, 3, 1386612161, "beta"}
+  {"3.0beta3", 3, 0, 3, 1387356188, "beta"}
 };
 
 static char *alanId =
