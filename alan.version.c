176cf31fd96c752ecb81a40d7b4132e2f16791b2
Thomas Nilsson <thomas@junovagen.se>
2014-01-10 21:52:19 +0000

    Minor change to Application Error message

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta4",
-  "Alan 3.0beta4 -- Adventure Language System (2014-01-10 22:39)",
+  "Alan 3.0beta4 -- Adventure Language System (2014-01-10 22:45)",
   "2014-01-10",
-  "22:39:54",
+  "22:45:55",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta4", 3, 0, 4, 1389389994, "beta"}
+  {"3.0beta4", 3, 0, 4, 1389390355, "beta"}
 };
 
 static char *alanId =
