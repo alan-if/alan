6dcf0067c2a8cbcb2eedd4f224749fd7a7c981fe
Thomas Nilsson <thomas@junovagen.se>
2014-01-13 07:57:51 +0000

    Update documentation about the containment loop messages

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta4",
-  "Alan 3.0beta4 -- Adventure Language System (2014-01-10 22:45)",
-  "2014-01-10",
-  "22:45:55",
+  "Alan 3.0beta4 -- Adventure Language System (2014-01-13 08:40)",
+  "2014-01-13",
+  "08:40:02",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta4", 3, 0, 4, 1389390355, "beta"}
+  {"3.0beta4", 3, 0, 4, 1389598802, "beta"}
 };
 
 static char *alanId =
--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta4",
-  "Alan 3.0beta4 -- Adventure Language System (2014-01-10 22:39)",
-  "2014-01-10",
-  "22:39:54",
+  "Alan 3.0beta4 -- Adventure Language System (2014-01-13 08:39)",
+  "2014-01-13",
+  "08:39:38",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta4", 3, 0, 4, 1389389994, "beta"}
+  {"3.0beta4", 3, 0, 4, 1389598778, "beta"}
 };
 
 static char *alanId =
