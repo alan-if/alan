d33583099b22a24fb6578afbace93a0a6040ce9a
Thomas Nilsson <thomas@junovagen.se>
2015-06-20 07:28:57 +0100

    reverted transitive here/nearby error detection, since it actually makes some sense

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta5",
-  "Alan 3.0beta5 -- Adventure Language System (2015-06-19 16:03)",
-  "2015-06-19",
-  "16:03:40",
+  "Alan 3.0beta5 -- Adventure Language System (2015-06-20 08:26)",
+  "2015-06-20",
+  "08:26:45",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta5", 3, 0, 5, 1434722620, "beta"}
+  {"3.0beta5", 3, 0, 5, 1434781605, "beta"}
 };
 
 static char *alanId =
