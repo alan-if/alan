ccef3a54bc8e34d642e0553b0a2e73108bf9169d
Thomas Nilsson <thomas@junovagen.se>
2015-06-21 21:14:25 +0100

    ...

--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta5",
-  "Alan 3.0beta5 -- Adventure Language System (2015-06-20 08:26)",
-  "2015-06-20",
-  "08:26:45",
+  "Alan 3.0beta5 -- Adventure Language System (2015-06-21 22:09)",
+  "2015-06-21",
+  "22:09:40",
   "Thomas",
   "",
   "cygwin32",
-  {"3.0beta5", 3, 0, 5, 1434781605, "beta"}
+  {"3.0beta5", 3, 0, 5, 1434917380, "beta"}
 };
 
 static char *alanId =
