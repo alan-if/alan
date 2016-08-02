f2e801668ad21d706f6aac64ec04120b6ee8854f
Thomas Nilsson <thomas@junovagen.se>
2016-08-02 19:42:11 +0200

    Update paths etc. because restoring on new Windows 10 machine...

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta5",
-  "Alan 3.0beta5 -- Adventure Language System (2015-08-25 07:38)",
-  "2015-08-25",
-  "07:38:04",
+  "Alan 3.0beta5 -- Adventure Language System (2016-08-02 18:48)",
+  "2016-08-02",
+  "18:48:18",
   "Thomas",
-  "",
+  "thoni64",
   "cygwin32",
-  {"3.0beta5", 3, 0, 5, 1440481084, "beta"}
+  {"3.0beta5", 3, 0, 5, 1470156498, "beta"}
 };
 
 static char *alanId =
