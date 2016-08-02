cbfb113f041c167e04d6b38c3d4620753e0e9a0a
Thomas Nilsson <thomas.nilsson@responsive.se>
2016-08-02 09:55:20 +0200

    Fix a problem with adding the hero, if the hero was also declared as a verb (which it might due to syntax error recovery inserting VERB)

--- compiler/ins_tests.c
+++ compiler/ins_tests.c
@@ -25,4 +25,17 @@ AfterEach(Instance) {}
 
 
 Ensure(Instance, can_ensure_hero_inherits_from_actor_when_hero_has_no_properties) {
+    Adventure *adventure = NEW(Adventure);
+    Symbol *verb_hero = NEW(Symbol);
+
+    actorSymbol = NEW(Symbol);
+
+    expect(lookup, when(idString, is_equal_to_string("hero")), will_return(verb_hero));
+    addHero(adventure);
+}
+
+TestSuite *ins_tests(void) {
+    TestSuite *suite = create_test_suite();
+    add_test_with_context(suite, Instance, can_ensure_hero_inherits_from_actor_when_hero_has_no_properties);
+    return suite;
 }
