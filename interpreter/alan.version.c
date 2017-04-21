4dbe335311726b9f277322b22128b2e83699b622
Thomas Nilefalk <thomas@junovagen.se>
2017-04-21 20:50:51 +0200

    Finalize building on 64-bit Cygwin

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta5",
-  "Alan 3.0beta5 -- Adventure Language System (2017-04-21 08:42)",
+  "Alan 3.0beta5 -- Adventure Language System (2017-04-21 17:20)",
   "2017-04-21",
-  "08:42:37",
+  "17:20:07",
   "Thomas",
   "thoni64",
   "cygwin32",
-  {"3.0beta5", 3, 0, 5, 1492756957, "beta"}
+  {"3.0beta5", 3, 0, 5, 1492788007, "beta"}
 };
 
 static char *alanId =
