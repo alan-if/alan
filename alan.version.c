a4b4a9bf9a5cb7d092390d02b7e52d92dfa343ff
Thomas Nilefalk <thomas@junovagen.se>
2017-04-27 20:02:55 +0200

    Updated venum generation

--- alan.version.c
+++ alan.version.c
@@ -6,14 +6,14 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta6",
-  "Alan 3.0beta6 -- Adventure Language System (2017-04-27 18:24)",
+  "Alan 3.0beta6 -- Adventure Language System (2017-04-27 19:02)",
   "2017-04-27",
-  "18:24:00",
+  "19:02:17",
   "Thomas",
   "THONI64",
   "mingw32",
-  {"3.0beta6", 3, 0, 6, 1493313840, "beta"}
+  {"3.0beta6", 3, 0, 6, 1493316137, "beta"}
 };
 
-static char *alanId =
+char *alanId =
   "@(#)RELEASE ";
--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -5,15 +5,15 @@
 Product alan = {
   "Alan",
   "Adventure Language System",
-  "Alan 3.0beta5",
-  "Alan 3.0beta5 -- Adventure Language System (2017-04-26 16:41)",
-  "2017-04-26",
-  "16:41:58",
+  "Alan 3.0beta6",
+  "Alan 3.0beta6 -- Adventure Language System (2017-04-27 19:02)",
+  "2017-04-27",
+  "19:02:17",
   "Thomas",
   "THONI64",
   "mingw32",
-  {"3.0beta5", 3, 0, 5, 1493221318, "beta"}
+  {"3.0beta6", 3, 0, 6, 1493316137, "beta"}
 };
 
-static char *alanId =
+char *alanId =
   "@(#)RELEASE ";
