f012ec4832d2eefe5df2bd672671c3b62793c105
Thomas Nilefalk <thomas@junovagen.se>
2018-05-22 08:35:21 +0200

    Fix CGREENLINKLIB for cygwin

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta6",
-  "Alan 3.0beta6 -- Adventure Language System (2018-01-16 21:45)",
-  "2018-01-16",
-  "21:45:37",
+  "Alan 3.0beta6 -- Adventure Language System (2018-05-22 07:31)",
+  "2018-05-22",
+  "07:31:03",
   "Thomas",
-  "thoni64",
-  "cygwin32",
-  {"3.0beta6", 3, 0, 6, 1516135537, "beta"}
+  "THONI64",
+  "mingw32",
+  {"3.0beta6", 3, 0, 6, 1526970663, "beta"}
 };
 
 char *alanId =
