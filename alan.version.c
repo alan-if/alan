114539567aad64786e3940882ce5e40f148cef3a
Thomas Nilefalk <thomas@junovagen.se>
2018-08-09 23:05:50 +0200

    Add the AsciiDOC:ed Guide to uploads

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta6",
-  "Alan 3.0beta6 -- Adventure Language System (2018-06-14 22:08)",
-  "2018-06-14",
-  "22:08:02",
+  "Alan 3.0beta6 -- Adventure Language System (2018-07-14 11:34)",
+  "2018-07-14",
+  "11:34:15",
   "Thomas",
   "THONI64",
   "mingw32",
-  {"3.0beta6", 3, 0, 6, 1529010482, "beta"}
+  {"3.0beta6", 3, 0, 6, 1531564455, "beta"}
 };
 
 char *alanId =
