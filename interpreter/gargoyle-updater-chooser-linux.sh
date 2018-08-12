fb81645e1d1d2a2bfaacd67f6bbd2e1fffbe9449
Thomas Nilefalk <thomas@nilefalk.se>
2018-08-12 20:20:51 +0200

    Update both /usr/games and /usr/lib/gargoyle if they exist, give error message if none does

--- interpreter/gargoyle-updater-chooser-linux.sh
+++ interpreter/gargoyle-updater-chooser-linux.sh
@@ -5,13 +5,26 @@
 #
 # It seems that Gargoyle might be installed in two different places:
 #
-# - /usr/games, and then all terps are there too
-# - /usr/lib/gargoyle, or at least this is where terps are stored
+# /usr/games
+#		and then all terps are there too (when installed from
+#		https://github.com/garglk/garglk)
 #
-# So we choose where to copy the new alan3
+# /usr/lib/gargoyle
+#       or at least this is where terps are stored, when installed as
+#		gargoyle-free - using a package manager
+#
+# So we copy the new alan3 to either or both of them
+found=0
 if [ -d /usr/lib/gargoyle ] ; then
     sudo cp alan3 /usr/lib/gargoyle
-else
-    sudo cp alan3 /usr/games
+    echo "Gargoyle in /usr/lib/gargoyle updated."
+    found=1
 fi
-echo "Gargoyle updated."
+if [ -f /usr/games/alan3 ] ; then
+    sudo cp alan3 /usr/games
+    echo "Gargoyle in /usr/games updated."
+    found=1
+fi
+if [ $found -eq 0 ] ; then
+    echo "No Gargoyle found in /usr/games or /usr/lib/gargoyle."
+fi
