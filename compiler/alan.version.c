aacabc61837a9568acb1d2720391ce1feed64891
Thomas Nilefalk <thomas@junovagen.se>
2018-11-02 18:05:25 +0100

    Fiddling with venum and NEXTRELEASE

--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta6",
-  "Alan 3.0beta6 -- Adventure Language System (2018-11-01 00:04)",
+  "Alan 3.0beta6 -- Adventure Language System (2018-11-01 13:57)",
   "2018-11-01",
-  "00:04:29",
+  "13:57:10",
   "Thomas",
   "thoni64",
-  "cygwin32",
-  {"3.0beta6", 3, 0, 6, 1541027069, "beta"}
+  "cygwin64",
+  {"3.0beta6", 3, 0, 6, 1541077030, "beta"}
 };
 
 char *alanId =
