ee8bf059fa2b26b5ecf07e442de6880ae2d205aa
Thomas Nilefalk <thomas@junovagen.se>
2019-05-07 23:42:41 +0200

    Fix another instance of status lines botching regression tests

--- regression/restore/restoreEvents.sh
+++ regression/restore/restoreEvents.sh
@@ -1,4 +1,4 @@
 ../../bin/alan restoreEvents
-../../bin/arun restoreEvents < restoreEvents1.input
-../../bin/arun restoreEvents < restoreEvents2.input
+../../bin/arun -r restoreEvents < restoreEvents1.input
+../../bin/arun -r restoreEvents < restoreEvents2.input
 rm restoreEvents.sav
