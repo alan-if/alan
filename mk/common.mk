370f9d7b6c505ef11de482c1ab67fd3ffd67931d
Thomas Nilefalk <thomas@junovagen.se>
2019-05-18 16:58:40 +0200

    Add file with common Makefile stuff

---
+++ mk/common.mk
@@ -0,0 +1,18 @@
+# Some common definitions for the Alan Makefiles
+
+# Discover OS and ARCH
+UNAME_O_WORKS=$(shell uname -o &>/dev/null; echo $$?)
+ifeq ($(UNAME_O_WORKS),0)
+  OS=$(shell uname -o)
+else
+  OS=$(shell uname -s)
+endif
+ifeq ($(OS),GNU/Linux)
+  OS=Linux
+endif
+ARCH = $(shell uname -m)
+$(info OS=$(OS), ARCH=$(ARCH))
+
+# Shorthands
+COMPILE = $(CC) $(CFLAGS)
+LINK = $(CC) $(LDFLAGS)
