60c29b94a7cb692761bae4de6b26ed9d6e26aa5c
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2020-01-21 13:49:53 +0100

    Fixup sed patterns for non-GNU

--- compiler/testing/arguments/summary.sh
+++ compiler/testing/arguments/summary.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash -i
-../../alan -summary ok.alan |  sed 's/[:=] *-\?[0-9]\+/: <number>/g'
+../../alan -summary ok.alan |  sed 's/ *[0-9][0-9]*/ <number>/g'
--- compiler/testing/arguments/verbose.sh
+++ compiler/testing/arguments/verbose.sh
@@ -1,1 +1,1 @@
-../../alan -verbose ok.alan | grep -v "Adventure Language Compiler" | sed 's/[0-9 ][\x08 0-9]\+$/ <number>/g'
+../../alan -verbose ok.alan | grep -v "Adventure Language Compiler" | tr -d '\b' | sed 's/: *[0-9][0-9 ]*$/: <number>/g' | sed 's/: *$/: <number>/g'
