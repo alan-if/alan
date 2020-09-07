35e0ffc12664a332b50a4fc12944caa97b57e574
Thomas Nilefalk <thomas@junovagen.se>
2020-09-07 22:17:59 +0200

    New version stamp

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta7",
-  "Alan 3.0beta7 -- Adventure Language System (2020-09-07 22:49)",
+  "Alan 3.0beta7 -- Adventure Language System (2020-09-07 22:13)",
   "2020-09-07",
-  "22:49:37",
+  "22:13:53",
   "Thomas",
   "thoni64",
   "cygwin64",
-  {"3.0beta7", 3, 0, 7, 1599511777, "beta"}
+  {"3.0beta7", 3, 0, 7, 1599509633, "beta"}
 };
 
 char *alanId =
