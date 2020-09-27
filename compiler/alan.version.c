ca19a2ccd66715db2b88c91ddc6ec2574de26d4b
Thomas Nilefalk <thomas@nilefalk.se>
2020-09-27 12:24:26 +0200

    Re-generate with new Makefile.tm

--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta8",
-  "Alan 3.0beta8 -- Adventure Language System (2020-09-26 21:37)",
-  "2020-09-26",
-  "21:37:49",
+  "Alan 3.0beta8 -- Adventure Language System (2020-09-27 12:25)",
+  "2020-09-27",
+  "12:25:23",
   "",
   "thoni64",
   "linux",
-  {"3.0beta8", 3, 0, 8, 1601149069, "beta"}
+  {"3.0beta8", 3, 0, 8, 1601202323, "beta"}
 };
 
 char *alanId =
