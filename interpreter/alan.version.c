3b318d121bfbeac480b3eb2526bc9fe43270f60f
Thomas Nilefalk <thomas@nilefalk.se>
2021-02-01 08:25:48 +0100

    Ensure -r overrides -v so that ifid test passes

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -1,4 +1,4 @@
-/* alan.c - Created by venum 4.2 beta */
+/* alan.version.c - Created by venum 4.2beta1 */
 
 #include "alan.version.h"
 
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta8",
-  "Alan 3.0beta8 -- Adventure Language System (2021-02-01 07:59)",
+  "Alan 3.0beta8 -- Adventure Language System (2021-02-01 08:22)",
   "2021-02-01",
-  "07:59:55",
+  "08:22:57",
   "Thomas",
   "thoni64",
-  "mingw64",
-  {"3.0beta8", 3, 0, 8, 1612162795, "beta"}
+  "cygwin64",
+  {"3.0beta8", 3, 0, 8, 1612164177, "beta"}
 };
 
 char *alanId =
