561b2b9f35ac72f7bb63e9b1605a9e223c66a8a9
Thomas Nilefalk <thomas@nilefalk.se>
2021-05-08 14:44:47 +0200

    [utf8][log] Call ensureExternalEncoding() for insert strings

--- compiler/lmlog.c
+++ compiler/lmlog.c
@@ -9,46 +9,48 @@
 #include "lmList.h"
 #include "util.h"
 #include "srcp_x.h"
+#include "converter.h"
 
 
 void lmlog(Srcp *pos, int ecode, lmSev sev, char *insert_strings) {
-    lmLog(pos, ecode, sev, insert_strings);
+    /* TODO: SYSERR of insert_strings contains lmSEPARATOR, should only be used if ONE insert string */
+    char *recoded = ensureExternalEncoding(insert_strings);
+    lmLog(pos, ecode, sev, recoded);
+    free(recoded);
 }
 
 
 void lmlogv(Srcp *pos, int ecode, lmSev sev, ...) {
     va_list ap;
-    char *errorstring = (char *)malloc(256);
-    int curlen = 256;
+    int allocated = 1;
+    char *insertstrings = (char *)malloc(allocated);
     int len = 0;
-    char *tmp, *arg;
+    char *arg;
 
     va_start(ap, sev);          /* sev is last non-vararg */
-    errorstring[0] = '\0';
+    insertstrings[0] = '\0';
     while ((arg = va_arg(ap, char *))) {
         if (len > 0) {
-            errorstring[len] = lmSEPARATOR;
-            errorstring[len+1] = '\0';
+            insertstrings[len] = lmSEPARATOR;
+            insertstrings[len+1] = '\0';
         }
-        if (len + strlen(arg) >= curlen) {
-            tmp = errorstring;
-            curlen += 256;
-            errorstring = (char *)malloc(curlen);
-            /* Check errorstring */
-            if (!errorstring) {
+        char *argument = ensureExternalEncoding(arg);
+        if (len + strlen(argument) >= allocated) {
+            int extension = strlen(argument)+2; /* lmSeparator + null */
+            allocated += extension;
+            insertstrings = (char *)realloc(insertstrings, allocated);
+            if (!insertstrings) {
                 SYSERR("Out of memory!", nulsrcp);
                 return;
             }
-            strcpy(errorstring, tmp);
-            free(tmp);
         }
-        strcat(errorstring, arg);
-        len = strlen(errorstring);
-
+        strcat(insertstrings, argument);
+        len = strlen(insertstrings);
+        free(argument);
     }
 
     va_end(ap);
 
-    lmLog(pos, ecode, sev, errorstring);
-    free(errorstring);
+    lmLog(pos, ecode, sev, insertstrings);
+    free(insertstrings);
 }
