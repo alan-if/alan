c355757ed2cef3373fd9dd888efc32b0352845f1
Thomas Nilefalk <thomas@nilefalk.se>
2021-05-16 18:23:03 +0200

    [code][cleanup] Move sysdep unittests to isolated

--- interpreter/sysdep_tests.c
+++ interpreter/sysdep_tests.c
@@ -15,6 +15,25 @@ BeforeEach(Sysdep) {
 AfterEach(Sysdep) {
 }
 
+Ensure(Sysdep, canCompareStringsIgnoringCase)
+{
+  assert_true(compareStrings("abcd", "abcd")==0);
+  assert_true(compareStrings("abcd", "Abcd")==0);
+  assert_true(compareStrings("abcd", "aBcd")==0);
+  assert_true(compareStrings("abcd", "abCd")==0);
+  assert_true(compareStrings("abcd", "abcD")==0);
+  assert_true(compareStrings("abcd", "abcd")==0);
+  assert_true(compareStrings("ABCD", "Abcd")==0);
+  assert_true(compareStrings("ABCD", "aBcd")==0);
+  assert_true(compareStrings("ABCD", "abCd")==0);
+  assert_true(compareStrings("ABCD", "abcD")==0);
+  assert_true(compareStrings("abcd", "abcde")!=0);
+  assert_true(compareStrings("abcde", "Abcd")!=0);
+  assert_true(compareStrings("abc", "aBcd")!=0);
+  assert_true(compareStrings("abd", "abCd")!=0);
+  assert_true(compareStrings("bcd", "abcD")!=0);
+}
+
 Ensure(Sysdep, returns_zero_for_equal_ascii_strings) {
     char *string1 = "abc";
 
