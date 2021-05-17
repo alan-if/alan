4451c16a0809bececfc1a92ef322878bbec390ba
Thomas Nilefalk <thomas@nilefalk.se>
2021-05-18 00:47:57 +0200

    [make][cleanup] Rename some Makefile symbols to LINKED_UNITTESTS_...

--- interpreter/sources.mk
+++ interpreter/sources.mk
@@ -5,9 +5,6 @@ include ../mk/include_guard.mk
 # CGreen unit test, stub and mocking framework by
 # Marcus Baker et al. (https://github.com/cgreen-devs/cgreen)
 
-# TODO: move all "old style" tests (xxxTests.c) to isolated
-# so that we can remove one type of linking and running
-
 # Either using its runner which discovers test automatically...
 # With everything mocked so they run in complete isolation...
 MODULES_WITH_ISOLATED_UNITTESTS = \
@@ -24,8 +21,13 @@ MODULES_WITH_ISOLATED_UNITTESTS = \
 	stack \
 	sysdep \
 
-# ... or in one library linked with all modules (much less clean... but better than ...)
-MODULES_WITH_UNITTESTS_USING_RUNNER = \
+# ... or in one library linked with all modules (much less clean...)
+# These have corresponding xxxTests.c which #include xxx.c to get at
+# the internals (inappropriate intimacy...). They are then compiled
+# and linked together with the non-unittested modules in a library
+# that can be cgreen-runner'd. They should be migrated to isolated
+# unittests...
+MODULES_WITH_LINKED_UNITTESTS = \
 	args.c \
 	AltInfo.c \
 	act.c \
@@ -76,26 +78,19 @@ MODULES_WITHOUT_UNITTESTS = \
 	fnmatch.c \
 	converter.c
 
-# All sources common for the main build
-MAINSRCS = \
-	$(MODULES_WITH_UNITTESTS_USING_RUNNER) \
-	$(MODULES_WITH_UNITTESTS_USING_MAIN) \
-	$(MODULES_WITHOUT_UNITTESTS)
 
-MODULES_WITH_UNITTESTS_USING_RUNNER_TESTSRCS = ${MODULES_WITH_UNITTESTS_USING_RUNNER:.c=Tests.c}
-MODULES_WITH_UNITTESTS_USING_MAIN_TESTSRCS = ${MODULES_WITH_UNITTESTS_USING_MAIN:.c=Tests.c}
+MODULES_WITH_LINKED_UNITTESTS_SRCS = ${MODULES_WITH_LINKED_UNITTESTS:.c=Tests.c}
 UNITTESTSMAIN = unittests.c xml_reporter.c gopt.c
 
-UNITTESTS_USING_MAIN_SRCS = $(UNITTESTSMAIN) $(MODULES_WITH_UNITTESTS_USING_RUNNER_TESTSRCS) $(MODULES_WITH_UNITTESTS_USING_MAIN_TESTSRCS) $(MODULES_WITHOUT_UNITTESTS)
-UNITTESTS_USING_RUNNER_SRCS = $(MODULES_WITH_UNITTESTS_USING_RUNNER_TESTSRCS) $(MODULES_WITH_UNITTESTS_USING_MAIN) $(MODULES_WITHOUT_UNITTESTS)
+LINKED_UNITTESTS_SRCS = $(MODULES_WITH_LINKED_UNITTESTS_SRCS) $(MODULES_WITHOUT_UNITTESTS)
 
-UNITTESTS_ALL_SRCS = $(MAINSRCS) $(MODULES_WITH_UNITTESTS_USING_MAIN_TESTSRCS) $(MODULES_WITH_UNITTESTS_USING_RUNNER_TESTSRCS) $(UNITTESTSMAIN)
+ALL_MODULES = $(MODULES_WITH_LINKED_UNITTESTS) $(MODULES_WITHOUT_UNITTESTS)
 
-ARUNSRCS = arun.c $(MAINSRCS)
+ARUNSRCS = arun.c $(ALL_MODULES)
 GLKSRCS = glkstart.c glkio.c
 WINARUNSRCS = ${GLKSRCS} ${ARUNSRCS} winglk.c
 GLKARUNSRCS = ${GLKSRCS} ${ARUNSRCS}
 GARARUNSRCS = ${GLKSRCS} ${ARUNSRCS}
 
 # Sources triggering new version marking
-VERSIONSRCS = $(MAINSRCS) arun.c glkstart.c glkio.c winglk.c
+VERSIONSRCS = $(ARUNSRCS) glkstart.c glkio.c winglk.c
