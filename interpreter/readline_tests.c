e6340aea34e70b53dae794dd7073bc072b704367
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2021-05-18 16:12:35 +0200

    [utf8][tdd] Add a unittest for delete last char

--- interpreter/readline_tests.c
+++ interpreter/readline_tests.c
@@ -34,13 +34,13 @@ Ensure(Readline, can_read_some_ascii_characters) {
            will_set_contents_of_parameter(buf, &stubbed_char, sizeof(char)),
            will_return(1),
            times(3));
+    /* ... and echo them */
     expect(mocked_read,
            will_set_contents_of_parameter(buf, &newline, sizeof(char)),
            will_return(1));
-    /* ... and echo them */
     expect(mocked_write,
            will_return(1),
-           times(4));
+           times(3));
 
     expect(ensureInternalEncoding,
            when(string, is_equal_to_string("aaa")),
@@ -48,4 +48,37 @@ Ensure(Readline, can_read_some_ascii_characters) {
 
     readline(buffer);
     assert_that(buffer, is_equal_to_string("aaa"));
+}
+
+
+
+Ensure(Readline, can_delete_one_ascii_character) {
+    char stubbed_char = 'a';
+    char delete = '\04';
+    char newline = '\n';
+    char buffer[100];
+
+    /* Expect to read 3 'a's, a delete and a newline... */
+    expect(mocked_read,
+           will_set_contents_of_parameter(buf, &stubbed_char, sizeof(char)),
+           will_return(1),
+           times(3));
+    /* ... and echo them */
+    expect(mocked_write,
+           will_return(1),
+           times(3));
+    /* Now delete last */
+    expect(mocked_read,
+           will_set_contents_of_parameter(buf, &delete, sizeof(char)),
+           will_return(1));
+    expect(mocked_read,
+           will_set_contents_of_parameter(buf, &newline, sizeof(char)),
+           will_return(1));
+
+    expect(ensureInternalEncoding,
+           when(string, is_equal_to_string("aa")),
+           will_return(strdup("aa"))); /* Because it should be malloc'ed */
+
+    readline(buffer);
+    assert_that(buffer, is_equal_to_string("aa"));
 }
