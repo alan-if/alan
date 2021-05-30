25e7eaf56b5db18357779acf81fc58a422501dba
Thomas Nilefalk <thomas@nilefalk.se>
2021-05-30 10:51:35 +0200

    [ci][buildnumbers] Ensure BUILDNUMBERS are used "everywhere"

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta8",
-  "Alan 3.0beta8 -- Adventure Language System (2021-04-28 09:12)",
-  "2021-04-28",
-  "09:12:21",
+  "Alan 3.0beta8 -- Adventure Language System (2021-05-30 09:15)",
+  "2021-05-30",
+  "09:15:27",
   "",
   "thoni64",
   "linux",
-  {"3.0beta8", 3, 0, 8, 1619593941, "beta"}
+  {"3.0beta8", 3, 0, 8, 1622358927, "beta"}
 };
 
 char *alanId =
