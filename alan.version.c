32dab780b065d87774530ba3a06c51f4d65b26f5
Thomas Nilefalk <thomas@nilefalk.se>
2021-08-10 21:24:48 +0200

    [timestamps] Updated

--- alan.version.c
+++ alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta8",
-  "Alan 3.0beta8 -- Adventure Language System (2021-08-03 11:57)",
-  "2021-08-03",
-  "11:57:51",
+  "Alan 3.0beta8 -- Adventure Language System (2021-08-10 20:31)",
+  "2021-08-10",
+  "20:31:41",
   "",
   "thoni64",
   "linux",
-  {"3.0beta8", 3, 0, 8, 1627984671, "beta"}
+  {"3.0beta8", 3, 0, 8, 1628620301, "beta"}
 };
 
 char *alanId =
--- compiler/alan.version.c
+++ compiler/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta8",
-  "Alan 3.0beta8 -- Adventure Language System (2021-08-02 08:52)",
-  "2021-08-02",
-  "08:52:52",
+  "Alan 3.0beta8 -- Adventure Language System (2021-08-08 20:29)",
+  "2021-08-08",
+  "20:29:46",
   "",
   "thoni64",
   "linux",
-  {"3.0beta8", 3, 0, 8, 1627887172, "beta"}
+  {"3.0beta8", 3, 0, 8, 1628447386, "beta"}
 };
 
 char *alanId =
