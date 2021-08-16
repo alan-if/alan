f1190d97697eb917c2f2fea06d1962fe6aa79a43
Thomas Nilefalk <thomas@nilefalk.se>
2021-08-16 18:18:02 +0200

    [timestamps] Updated

--- interpreter/alan.version.c
+++ interpreter/alan.version.c
@@ -6,13 +6,13 @@ Product alan = {
   "Alan",
   "Adventure Language System",
   "Alan 3.0beta8",
-  "Alan 3.0beta8 -- Adventure Language System (2021-08-15 20:59)",
-  "2021-08-15",
-  "20:59:20",
+  "Alan 3.0beta8 -- Adventure Language System (2021-08-16 18:14)",
+  "2021-08-16",
+  "18:14:05",
   "",
   "thoni64",
   "linux",
-  {"3.0beta8", 3, 0, 8, 1629053960, "beta"}
+  {"3.0beta8", 3, 0, 8, 1629130445, "beta"}
 };
 
 char *alanId =
